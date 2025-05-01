02cf5e09809a3533e376731b53abb102827288de
Yannick <yannick-luca.ruettgers@bayer.com>
2025-05-01 22:03:48 +0200

    Added API Gateway & Lambda

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -31,3 +31,8 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "user_creation_lambda_package" {
+  description = "Path to the deployment package for the user creation Lambda function."
+  type        = string
+}
